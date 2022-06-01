ff58c97a43c099ce1028c28cd8db947d383010bb
Matusevich Vitaliy <vimatu@gmail.com>
2022-06-01 18:23:52 +0300

    feat: add create fs

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,22 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { writeFile, access } from 'fs/promises';
+import { constants } from 'fs';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const dirName = 'files';
+const newDirName = `${dirName}_copy`;
+const defaultFileName = 'fresh.txt';
+const defaultContent = 'I am fresh and young';
+const errorMessage = 'FS operation failed';
+
+const checkExist
+
+
 export const copy = async () => {
     // Write your code here 
+
+
 };
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,23 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { writeFile, access } from 'fs/promises';
+import { constants } from 'fs';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const defaultFileName = 'fresh.txt';
+const defaultContent = 'I am fresh and young';
+const errorMessage = 'FS operation failed';
+
 export const create = async () => {
-    // Write your code here 
+  // Write your code here
+  const newFilePath = path.resolve(__dirname, `./files/${defaultFileName}`);
+  try {
+    await access(newFilePath, constants.F_OK);
+  } catch (error) {
+    await writeFile(newFilePath, defaultContent, { encoding: 'utf-8' });
+    return;
+  }
+  throw new Error(errorMessage);
 };
