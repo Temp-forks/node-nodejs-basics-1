edb601f220d2815d9421ef1a018fb262ede7e73a
Matusevich Vitaliy <vimatu@gmail.com>
2022-06-03 11:40:59 +0300

    feat: implement cli/env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,10 @@
+const prefix = 'RSS_';
+
 export const parseEnv = () => {
-    // Write your code here 
+  // Write your code here
+  const variableArray = Object.entries(process.env).filter((entry) => entry[0].indexOf(prefix) === 0);
+  const array = variableArray.map((entry) => entry.join('='));
+  console.log(array.join('; '));
 };
+
+parseEnv();
