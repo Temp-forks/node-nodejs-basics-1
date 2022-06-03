580325fee4b46856c3b3aa85b4b455e8cbc12302
Matusevich Vitaliy <vimatu@gmail.com>
2022-06-03 14:37:25 +0300

    feat: implement cli/args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,17 @@
+const keyReg = /^--\w+/;
+
 export const parseArgs = () => {
-    // Write your code here 
+  // Write your code here
+  const argsArray = process.argv.slice(2).reduce((accArray, arg) => {
+    if (arg.match(keyReg)) {
+      accArray.push([arg, '']);
+    } else if (accArray.length) {
+      const entry = accArray[accArray.length - 1];
+      entry[1] = `${entry[1]} ${arg}`;
+    }
+    return accArray;
+  }, []);
+
+  const array = argsArray.map((entry) => `${entry[0].slice(2)} is${entry[1]}`);
+  console.log(array.join(', '));
 };
